cbe4936336cc1ef44ed918c0dfc01f561818d301
fbzr <fabricio.spb@gmail.com>
2020-03-02 11:13:25 -0800

    create sign up form, add fields and yup validation

--- react-1/src/components/SignUp.jsx
+++ react-1/src/components/SignUp.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Form, withFormik, Field } from 'formik';
+import { Form, withFormik, useField } from 'formik';
 import * as yup from 'yup';
+import { TextField } from '@material-ui/core';
 
-
-const SignUpForm = () => {
+// Material UI TextField with access to Formik Field's props and methods 
+const MuiFormikTextField = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
     return (
-        <div>
-            Sign Up
-        </div>
+        <TextField 
+            {...field}
+            {...props}
+            label={label}
+            error={meta.error && meta.touched}
+            helperText={ (meta.error && meta.touched) && meta.error }
+        />         
+    )
+}
+
+const SignUpForm = ({ values }) => {
+    return (
+        <Form autoComplete='off'>
+            <MuiFormikTextField label='First Name' name='fname' id='fname' />
+            <MuiFormikTextField label='Last Name' name='lname' id='lname' />
+            <MuiFormikTextField label='Email' name='email' id='email' type='email' />
+            <MuiFormikTextField name='password' id='password' type='password' label='Password' />
+            <MuiFormikTextField name='password2' id='password2' type='password' label='Password Confirmation' />
+            
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Form>
     )
 }
 
@@ -16,12 +36,17 @@ const SignUp = withFormik({
     mapPropToValues: () => ({
         fname: '',
         lname: '',
-        email: ''
+        email: '',
+        password: '',
+        password2: ''
     }),
+    // Create yup validation schema
     validationSchema: yup.object().shape({
-        fname: yup.string().min(2, 'First name needs to have at least 2 characters').required('First name is required'),
-        lname: yup.string().min(2, 'First name needs to have at least 2 characters').required('First name is required'),
-        email: yup.string().email().required('Email is required')
+        fname: yup.string().min(2, 'First name must have at least 2 characters').required('First name is required'),
+        lname: yup.string().min(2, 'First name must have at least 2 characters').required('First name is required'),
+        email: yup.string().email().required('Email is required'),
+        password: yup.string().min(8, 'Password must have at least 8 characters').required('Password required'),
+        password2: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
     })
 })(SignUpForm)
 
