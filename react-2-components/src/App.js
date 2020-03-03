16f7cf31ae4b9f62d4813cc9970df21c5b799f2a
dlray89 <dlrayjr89@gmail.com>
2020-03-02 21:15:08 -0500

    update

--- react-2-components/src/App.js
+++ react-2-components/src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withFormik } from "formik";
+import * as Yup from "yup";
+import { withStyles } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import "./App.css";
 
-function App() {
+const styles = () => ({
+  card: {
+    maxWidth: 420,
+    marginTop: 50,
+  },
+  container: {
+    display: "Flex",
+    justifyContent: "center",
+  },
+  actions: {
+    float: "right"
+  }
+});
+
+const form = props => {
+  const {
+    classes,
+    values,
+    touched,
+    errors,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleReset
+  } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+  
+      <form onSubmit={handleSubmit}>
+      
+        <Card className={classes.card}>
+      <h1>Sauti</h1>
+
+          <CardContent>
+            <TextField
+              id="email"
+              label="Email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={touched.email ? errors.email : ""}
+              error={touched.email && Boolean(errors.email)}
+              style={{color: "#870000"}}
+
+              margin="dense"
+              variant="outlined"
+              fullWidth
+            />
+
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={touched.password ? errors.password : ""}
+              error={touched.password && Boolean(errors.password)}
+              style={{color: "#870000"}}
+              margin="dense"
+              variant="outlined"
+              fullWidth
+            />
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Button type="submit" color="primary" disabled={isSubmitting}>
+              LOGIN
+            </Button>
+            <Button color="secondary" onClick={handleReset}>
+              CLEAR
+            </Button>
+            
+          </CardActions>
+          <div className="link"><Link>Create an account</Link></div>
+         
+        </Card>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+const Form = withFormik({
+  mapPropsToValues: ({
+    email,
+    password
+  }) => {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must contain at least 8 characters")
+      .required("Enter your password")
+  }),
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      // submit to the server
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  }
+})(form);
+
+export default withStyles(styles)(Form);
