0a5aab4d9948ca46121dcefe123f4beaf5abcad2
fbzr <fabricio.spb@gmail.com>
2020-03-05 19:02:33 -0800

    add location filter to listings

--- the-sauti-project/src/components/Listings.jsx
+++ the-sauti-project/src/components/Listings.jsx
@@ -15,44 +15,15 @@ const useStyles = makeStyles(theme => ({
     }
   }));
 
-const Listings = () => {
-    const token = sessionStorage.getItem('token');
 
+const Listings = () => {
     const classes = useStyles();
     const [listings, setListings] = useState([]);
+    const [filteredListings, setFilteredListings] = useState([]);
     const [users, setUsers] = useState([]);
-    const [selectedUser, setSelectedUser] = useState({
-        id: '',
-        username: ''
-    });
-
-    // Fetches data when user selects an user
-    useEffect(() => {
-        const config = { headers: { 'Authorization':token } }
-        const fetchAllListings = async () => {            
-            try {
-                const res = await Axios.get('http://africanmarketplace.ddns.net:5000/api/listings');                    
-                setListings(res.data);
-            } catch(err) {
-                console.log(err.message);
-            }
-        }
-
-        const fetchUserListings = async () => {
-            try {
-                const res = await Axios.get(`http://africanmarketplace.ddns.net:5000/api/users/${selectedUser.id}/listings`, config);
-                setListings(res.data);
-            } catch(err) {
-                console.log(err.message);
-            }
-        }
-
-        if(selectedUser.id === '') {
-            fetchAllListings();
-        } else {
-            fetchUserListings();
-        }
-    }, [selectedUser]);
+    const [locations, setLocations] = useState([]);
+    const [selectedUser, setSelectedUser] = useState({ id: '', username: '' });
+    const [selectedLocation, setSelectedLocation] = useState('');
 
     useEffect(() => {
         // Fetch users for the select input
@@ -64,26 +35,70 @@ const Listings = () => {
                 console.log(err.message);
             }
         }
+
+        const fetchAllListings = async () => {            
+            try {
+                const res = await Axios.get('http://africanmarketplace.ddns.net:5000/api/listings');                    
+                setListings(res.data);
+            } catch(err) {
+                console.log(err.message);
+            }
+        }
+
         fetchUsers();
+        fetchAllListings();
     }, [])
 
-    const handleChange = e => setSelectedUser(e.target.value);
+    // Load locations select dropdown
+    useEffect(() => {
+        setLocations(listings
+            .filter((current, index) => listings.findIndex(item => item.location.toLowerCase() === current.location.toLowerCase()) === index)
+            .map(obj => obj.location));
+    }, [listings])
+
+    // Fetches data when user selects a filter
+    useEffect(() => {
+        let res = listings;
+        if( selectedLocation !== '' ) {
+            res = res.filter(listing => listing.location.toLowerCase() === selectedLocation.toLowerCase());
+        }
+        if( selectedUser.id !== '') {
+            res = res.filter(listing => listing.user_id === selectedUser.id);
+        }
+        setFilteredListings(res);
+    }, [selectedUser, selectedLocation, listings]);
+
+    const handleUserChange = e => setSelectedUser(e.target.value);
+    const handleLocationChange = e => setSelectedLocation(e.target.value);
     
     return (
         <Container className={classes.root}>
-            <FormControl className={classes.formControl}>
-                <InputLabel id="user-select-label">Select User</InputLabel>
-                <Select value={selectedUser.id === '' ? '' : selectedUser} labelId='user-select-label' displayEmpty onChange={handleChange}>
-                    <MenuItem value={{id: ''}}>All Listings</MenuItem>
-                    {users.map(user => (
-                        <MenuItem key={user.id} value={user}>
-                            {user.username}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+            <Grid container>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="user-select-label">Select User</InputLabel>
+                    <Select value={selectedUser.id === '' ? '' : selectedUser} labelId='user-select-label' displayEmpty onChange={handleUserChange}>
+                        <MenuItem value={{id: ''}}>All Users</MenuItem>
+                        {users.map(user => (
+                            <MenuItem key={user.id} value={user}>
+                                {user.username}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="location-select-label">Locations</InputLabel>
+                    <Select value={selectedLocation === '' ? '' : selectedLocation} labelId='location-select-label' displayEmpty={false} onChange={handleLocationChange}>
+                        <MenuItem value=''>All Locations</MenuItem>
+                        {locations.map(location => (
+                            <MenuItem key={location} value={location}>
+                                {location}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Grid>
             <Grid justify='flex-start' container>
-                { listings.map(item => (
+                { filteredListings.map(item => (
                     <ListingItem key={item.id} listing={item} />
                 )) }
             </Grid>
